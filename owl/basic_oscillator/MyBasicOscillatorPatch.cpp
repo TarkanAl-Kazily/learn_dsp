fce0bd802b975147ee2ef28ed8931d96f297a0de
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2023-04-23 22:13:11 -0700

    Improved the comments in my basic oscillator patch

--- owl/basic_oscillator/MyBasicOscillatorPatch.cpp
+++ owl/basic_oscillator/MyBasicOscillatorPatch.cpp
@@ -1,48 +1,58 @@
-/*
- LICENSE:
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+/**
+ * @file MyBasicOscillatorPatch.cpp
+ * @brief Implements a basic square oscillator with built in AR based VCA.
+ *
+ * Copyright (C) 2023 Tarkan Al-Kazily
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-
-/* created by the OWL team 2013 */
 
 #include "MyBasicOscillatorPatch.hpp"
 
+// For fast_exp2f
 #include "basicmaths.h"
 
 namespace {
 
-// 
-
-
+/**
+ * @brief Helper function that converts a voltage value to a frequency
+ *
+ * This can be replaced by the VoltsPerOctave::voltsToHertz function for the OWL equivalent.
+ */
 inline float getFreqFromVoltage(float voltage) {
     // https://gahlorddewald.com/hz-to-cv.html
+    // TLDR; Musical tones are logarithmic in which each octave is double the
+    // previous pitch in Hz.  Therefore in order to convert between the linear
+    // volt per octave to a frequency, we must use an exponential.
+
     // Base note to use for tuning (A4)
-    constexpr float kBaseTuning = 440.0;
-    // How many octaves below base note can be reached
-    constexpr float kOctaveAdjust = 5.0;
-    // Manual adjustment for the knob so C is approximately in the center
-    constexpr float kAdjustment = 0.68;
+    constexpr float kBaseTuning = 440.0f;
+    // What input voltage should be A4
+    constexpr float kOctaveAdjust = 5.0f;
+    // Additional adjustment for the knob so C is approximately in the center
+    constexpr float kAdjustment = 0.68f;
 
     return kBaseTuning * fast_exp2f(-kOctaveAdjust + kAdjustment + voltage);
 }
 
 } // anonymous namespace
 
-void MyBasicOscillatorPatch::_handle_parameters(float left_voltage) {
+void MyBasicOscillatorPatch::_handle_parameters() {
+    float left_voltage = calib.sampleToVolts(left[0]);
+
     // Set the frequency from the first parameter (detune)
+    // Casting a parameter (detune) to a value gets its value
     float freq = getFreqFromVoltage(left_voltage + detune);
     oscillator->setFrequency(freq);
 
@@ -54,10 +64,8 @@ void MyBasicOscillatorPatch::_handle_parameters(float left_voltage) {
 void MyBasicOscillatorPatch::processAudio(AudioBuffer &buffer) {
     FloatArray left = buffer.getSamples(LEFT_CHANNEL);
 
-    float left_voltage = calib.sampleToVolts(left[0]);
-
     // Handle parameters for oscillator
-    _handle_parameters(left_voltage);
+    _handle_parameters();
 
     // Generate audio
     oscillator->generate(left);
--- owl/basic_oscillator/MyBasicOscillatorPatch.hpp
+++ owl/basic_oscillator/MyBasicOscillatorPatch.hpp
@@ -1,22 +1,22 @@
-/*
- LICENSE:
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+/**
+ * @file MyBasicOscillatorPatch.hpp
+ * @brief Implements a basic square oscillator with built in AR based VCA.
+ *
+ * Copyright (C) 2023 Tarkan Al-Kazily
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
-
-/* created by the OWL team 2013 */
 
 #pragma once
 
@@ -25,39 +25,81 @@
 #include "SquareWaveOscillator.h"
 #include "AdsrEnvelope.h"
 
+/**
+ * @brief Implements a basic square oscillator with a built in envelope (Attack Release).
+ *
+ * Inputs, Outputs:
+ * [In, Left] Volt/Octave input to control the pitch of the oscillator.
+ *      Expects calibration to be done for the OWL module.
+ * [Button / Trigger 1] Gate input for envelope.
+ * [Out, Left] Audio out for module.
+ *
+ * Parameters:
+ * [A] Detune, which can be used to tune the oscillator within the octave.
+ * [B] Attack time in seconds. Ranges between 0 and 4 seconds.
+ * [C] Release time in seconds. Ranges between 0 and 4 seconds.
+ */
 class MyBasicOscillatorPatch : public Patch {
-public:
-  MyBasicOscillatorPatch() {
-    detune = getParameter("Detune", 0.5); // A
-    attack = getFloatParameter("Attack", 0.0, 4.0, 0.0); // B
-    decay = getFloatParameter("Release", 0.0, 4.0, 0.0); // C
+  public:
+    /**
+     * Constructor for the patch.
+     */
+    MyBasicOscillatorPatch() : calib(true) {
+        // The order these `getParameter` calls are made in determines which Parameter
+        // is assigned to which input (A, B, C).
+        detune = getParameter("Detune", 0.5); // A
+        attack = getFloatParameter("Attack", 0.0, 4.0, 0.0); // B
+        decay = getFloatParameter("Release", 0.0, 4.0, 0.0); // C
 
-    // Create / allocate all memory in the constructor
-    oscillator = SquareWaveOscillator::create(getSampleRate());
-    env = LinearAdsrEnvelope::create(getSampleRate());
-  }
+        // Allocate all memory in the constructor
+        oscillator = SquareWaveOscillator::create(getSampleRate());
+        env = LinearAdsrEnvelope::create(getSampleRate());
+    }
 
-  ~MyBasicOscillatorPatch() {
-    // Delete all memory in the constructor
-    SquareWaveOscillator::destroy(oscillator);
-    LinearAdsrEnvelope::destroy(env);
-  }
- 
-  void processAudio(AudioBuffer &buffer) override;
+    /**
+     * Destructor.
+     */
+    ~MyBasicOscillatorPatch() {
+        // Delete all memory in the constructor
+        SquareWaveOscillator::destroy(oscillator);
+        LinearAdsrEnvelope::destroy(env);
+    }
 
-  void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples) override;
+    /**
+     * @brief Main event loop for OWL patches.
+     *
+     * All patches must implement processAudio, and this patch is no different.
+     * This function handles changes from parameters before processing and generating audio.
+     */
+    void processAudio(AudioBuffer &buffer) override;
 
-private:
-  SquareWaveOscillator *oscillator;
+    /**
+     * @brief Callback for handling gate / button press events.
+     *
+     * The two gate inputs are special, and can be handled asynchronously through this callback.
+     * This is where the envelope state is updated based on the gate input.
+     */
+    void buttonChanged(PatchButtonId bid, uint16_t value, uint16_t samples) override;
 
-  FloatParameter detune;
-  FloatParameter attack;
-  FloatParameter decay;
+  private:
+    // The oscillator used to generate audio
+    SquareWaveOscillator *oscillator;
 
-  VoltsPerOctave calib;
+    // Input parameters used to shape the sound
+    FloatParameter detune;
+    FloatParameter attack;
+    FloatParameter decay;
 
-  // Called in processAudio to update all the parameters before generating audio
-  void _handle_parameters(float left_voltage);
+    // Supports applying offset and multiplication to [-1, 1] signals to a raw voltage level.
+    // This is constructed (above) to be associated with the "input", and pulls the stored
+    // calibration constants from system memory.
+    VoltsPerOctave calib;
 
-  LinearAdsrEnvelope *env;
+    // This is a helper function, called in processAudio, to update all parameters before generating audio
+    void _handle_parameters();
+
+    // This ADSR object is used to shape the module's sound in and out.
+    // Only the Attack and Release stages are set to non-default values, making it a
+    // linear Attack Release envelope.
+    LinearAdsrEnvelope *env;
 };
